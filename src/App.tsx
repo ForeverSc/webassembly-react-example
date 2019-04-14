e70758e9a9d59775e67d7b8886dabe3f37617db8
丰硕 <fengshuochn@126.com>
2019-04-14 19:03:30 +0800

    feat: init project config

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, FormEvent, ChangeEvent } from 'react'
+import './App.css'
+import wasmAdd from './add.wasm'
 
 class App extends Component {
-  render() {
+  public state = {
+    answer: 0,
+    a: 0,
+    b: 0
+  }
+
+  private handleWasmAddClick = async () => {
+    const { instance, module } = await wasmAdd()
+    const { add } = instance.exports
+    const { a, b } = this.state;
+
+    this.setState({
+      answer: add(a, b)
+    })
+  }
+
+  private handleChangeAValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      a: e.target.value
+    })
+  }
+
+  private handleChangeBValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      b: e.target.value
+    })
+  }
+
+  public render() {
+    const {
+      answer,
+      a,
+      b
+    } = this.state
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>
+          a:
+          <input type="text" value={a} onChange={this.handleChangeAValue}/>
+        </p>
+        <p>
+          b:
+          <input type="text" value={b} onChange={this.handleChangeBValue}/>
+        </p>
+        <p>answer: {answer}</p>
+        <button onClick={this.handleWasmAddClick}>click me to show wasm add answer</button>
       </div>
     );
   }
 }
 
-export default App;
+export default App
